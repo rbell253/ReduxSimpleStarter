6d33c212bff6a53eeae866d9804ef94b8fbe61d6
rbell253 <42948600+rbell253@users.noreply.github.com>
2018-09-05 07:49:58 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+Import React from 'react';
+Import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce
 // some HTML
 //const is declaring a variable(over var) because it is saying that this variable
 // will never change
+// can use => in place of "function" to declare a function
 const App = function() {
   return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-React.render(App);
+ReactDOM.render(<App />, document.querySelector('container'));
