85e6ccd7792e7f3bb4acb8db2320481e5a4a5502
rbell253 <42948600+rbell253@users.noreply.github.com>
2018-09-05 08:19:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 Import React from 'react';
 Import ReactDOM from 'react-dom';
 
-Import SearchBar from 'search_bar';
+Import SearchBar from './components/search_bar';
 
 // declare our youtube api key
 const API_KEY = ''
@@ -12,9 +12,13 @@ const API_KEY = ''
 // will never change
 // can use => in place of "function" to declare a function
 const App = function() {
-  return <div>Hi!</div>;
+  return(
+    <div>
+     <SearchBar />
+    </div>;
+   );
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
