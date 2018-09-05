93792fc75686a7f6481d25559c32cfe511bdea1e
rbell253 <42948600+rbell253@users.noreply.github.com>
2018-09-05 09:34:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-Import React from 'react';
-Import ReactDOM from 'react-dom';
-
-Import SearchBar from './components/search_bar';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 // declare our youtube api key
 const API_KEY = ''
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce
 // some HTML
 // const is declaring a variable(over var) because it is saying that this variable
 // will never change
 // can use => in place of "function" to declare a function
-const App = function() {
-  return(
-    <div>
-     <SearchBar />
-    </div>;
-   );
+class App extend component { 
+  render() {
+    return (
+      <div>
+       <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
