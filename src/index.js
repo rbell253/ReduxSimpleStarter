aeab7230dc302b3597d80e463a69b7f2b02f47af
rbell253 <42948600+rbell253@users.noreply.github.com>
2018-09-05 11:06:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 // declare our youtube api key
 const API_KEY = ''
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
 // Create a new component. This component should produce
 // some HTML
 // const is declaring a variable(over var) because it is saying that this variable
 // will never change
 // can use => in place of "function" to declare a function
-class App extend component { 
+class App extends component { 
+  constructor(props) {
+    super(props);
+    
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+    
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ 
+        videos: videos, 
+        selectedVideo: videos[0]
+      });
+    });
+  }
   render() {
     return (
       <div>
        <SearchBar />
+       <VideoDetail video={this.state.selectedVideo} />
+       <VideoList 
+        onVIdeoSelect={selectedVideo => this.setState({selectedVideo}) }  
+        videos={this.state.videos} />
       </div>
     );
   }
